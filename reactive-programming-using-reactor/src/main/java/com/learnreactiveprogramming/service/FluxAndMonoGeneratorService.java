f956ee974a42df0e99f94953984df2e4e5994fd6
Roman92P <roman.paszkow@gmail.com>
2023-09-22 23:01:31 +0200

    some changes

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,21 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+
+       return Flux.fromIterable(List.of("Alex", "Ben", "Chloe" ));
+
+    }
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(System.out::println);
+    }
 }
