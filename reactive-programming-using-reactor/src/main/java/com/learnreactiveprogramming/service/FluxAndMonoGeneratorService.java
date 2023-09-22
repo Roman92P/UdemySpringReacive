175c55354c39b4b3372bcd599ab542f40801d34b
Roman92P <roman.paszkow@gmail.com>
2023-09-22 23:48:09 +0200

    some changes

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,8 +1,12 @@
 package com.learnreactiveprogramming.service;
 
+import lombok.var;
+import org.reactivestreams.Publisher;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.Random;
 
 
 public class FluxAndMonoGeneratorService {
@@ -12,6 +16,33 @@ public class FluxAndMonoGeneratorService {
        return Flux.fromIterable(List.of("Alex", "Ben", "Chloe" ));
 
     }
+
+    public Flux<String> namesFlux_flatmap_async(int stringLength) {
+
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > stringLength)
+                .flatMap(this::splitString_withDelay)
+                .log();
+    }
+
+    public Flux<String> namesFlux_concatMap(int stringLength) {
+
+        return Flux.fromIterable(List.of("alex", "ben", "chloe"))
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > stringLength)
+                .concatMap(this::splitString_withDelay)
+                .log();
+    }
+
+    private Flux<String> splitString_withDelay(String s) {
+        var split = s.split("");
+        var delay = new Random().nextInt(1000);
+        return Flux.fromArray(split)
+                .delayElements(Duration.ofMillis(delay));
+
+    }
+
     public static void main(String[] args) {
 
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,25 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+    @Test
+    void namesFlux_concatMap() {
+        //given
+        int stringLength = 3;
+
+        //when
+        Flux<String> stringFlux = fluxAndMonoGeneratorService.namesFlux_concatMap(3);
+
+        //then
+        StepVerifier.create(stringFlux)
+                .expectNext("A","L","E","X", "C","H","L","O","E")
+                .verifyComplete();
+    }
 }
