211927a2cb8f1d4a130834bc4e1fb9f0da6b3261
Roman92P <roman.paszkow@gmail.com>
2023-09-23 23:15:26 +0200

    added first webflux test

--- movie-info-service/movie-info-service/src/test/unit/com/reactivespring/movieinfoservice/controller/FluxAndMonoControllerTest.java
+++ movie-info-service/movie-info-service/src/test/unit/com/reactivespring/movieinfoservice/controller/FluxAndMonoControllerTest.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,5 +30,38 @@ class FluxAndMonoControllerTest {
                 .is2xxSuccessful()
                 .expectBodyList(Integer.class)
                 .hasSize(3);
+    }
+
+    @Test
+    void flux_approach2() {
+
+        Flux<Integer> responseBody = webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .returnResult(Integer.class)
+                .getResponseBody();
+
+        StepVerifier.create(responseBody)
+                .expectNext(1,2,3)
+                .verifyComplete();
+    }
+
+    @Test
+    void flux_approach3() {
+
+        Flux<Integer> responseBody = webTestClient.get()
+                .uri("/flux")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+
+                .expectBodyList(Integer.class)
+                .consumeWith(listEntityExchangeResult -> {
+                    var responseBody1 = listEntityExchangeResult.getResponseBody();
+                    assert Objects.requireNonNull(responseBody1).size() == 3;
+                });
+
     }
 }
